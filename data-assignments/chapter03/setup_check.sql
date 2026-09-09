9b549f6dd0ad8b34ad223deaae6a40ae7700ac41
윤강 <yg920@naver.com>
2026-09-09 17:13:28 +0900

    26.9.9.TIL

---
+++ data-assignments/chapter03/setup_check.sql
@@ -0,0 +1,35 @@
+SELECT version();
+
+SELECT current_database();
+
+SELECT current_user;
+
+SELECT current_schema();
+
+SHOW search_path;
+
+
+SELECT 1 + 1 AS result;
+
+
+SELECT version();
+
+SELECT current_database();
+
+SELECT current_user;
+
+SELECT current_schema();
+
+SHOW search_path;
+
+SHOW transaction_read_only;
+
+SHOW TimeZone;
+
+
+SELECT current_database();
+
+
+SELECT 'A' AS step;
+SELECT 'B' AS step;
+SELECT 'C' AS step;
